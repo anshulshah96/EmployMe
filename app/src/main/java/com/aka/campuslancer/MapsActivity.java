8588eaf42a5d8baec8cdda842fbb58f15343f241
Manthan-R-Sheth <manthanrsheth96@gmail.com>
2015-12-05 18:59:01 +0530

    current location in map added

--- app/src/main/java/com/aka/campuslancer/MapsActivity.java
+++ app/src/main/java/com/aka/campuslancer/MapsActivity.java
@@ -1,40 +1,81 @@
 package com.aka.campuslancer;
 
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.ActionBarActivity;
 import android.widget.Toast;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapsActivity extends FragmentActivity
-        implements OnMapReadyCallback, GoogleMap.OnMapClickListener {
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, GoogleMap.OnMapClickListener,LocationListener {
+    GoogleMap map;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //  setContentView(R.layout.activity_map);
-        try {
             setContentView(R.layout.activity_maps);
-        } catch (Exception e) {
-            Toast.makeText(MapsActivity.this, e.toString(), Toast.LENGTH_LONG).show();
-            MapFragment mapFragment = (MapFragment) getFragmentManager()
+        MapFragment mapFragment = (MapFragment) getFragmentManager()
                     .findFragmentById(R.id.map);
-            //  mapFragment.getMapAsync(this);
-        }
+            mapFragment.getMapAsync(this);
+            map=mapFragment.getMap();
+            map.setMyLocationEnabled(true);
+
+            LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        Criteria criteria = new Criteria();
+        String bestp= locationManager.getBestProvider(criteria,true);
+        Location location=locationManager.getLastKnownLocation(bestp);
+            if(location!=null)
+            {
+                onLocationChanged(location);
+            }
+            locationManager.requestLocationUpdates(bestp,20000,0,this);
     }
+
+
     @Override
     public void onMapReady (GoogleMap map){
-        map.addMarker(new MarkerOptions()
-                .position(new LatLng(29.8662184, 77.89531809))
-                .title("Marker").draggable(true));
-        map.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+
     }
     @Override
     public void onMapClick (LatLng latLng){
         Toast.makeText(getApplicationContext(), "" + latLng.latitude, Toast.LENGTH_LONG).show();
         Toast.makeText(getApplicationContext(), "" + latLng.longitude, Toast.LENGTH_LONG).show();
-    }     }
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        double lat=location.getLatitude();
+        double longi=location.getLongitude();
+        LatLng latLng=new LatLng(lat,longi);
+        map.addMarker(new MarkerOptions().position(latLng)
+                .title("workplace").draggable(true));
+        map.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+        map.animateCamera(CameraUpdateFactory.zoomTo(15));
+
+    }
+
+
+}
